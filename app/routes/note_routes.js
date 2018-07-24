9792d60575c451fa84bbd64d137f203de69d6ea8
Aliaksader Zamirovski <studiaformanta@yandex.ru>
2018-07-24 12:12:40 +0300

    equipment item CRUD

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,6 +1,61 @@
+var ObjectID = require('mongodb').ObjectID;
+
 module.exports = function(app, db) {
-  app.post('/notes', (req, res) => {
-    console.log(req.body);
-    res.send('Hello');
+  app.get('/lab-equip/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('lab-equip').findOne(details, (err, item) => {
+      if (err) {
+        res.send({ 'error': 'An error occurred'});
+      } else {
+        res.send(item);
+      }      
+    });
+  });
+
+  app.post('/lab-equip', (req, res) => {
+    const equipment = { 
+      inventoryNumber: req.body.inventoryNumber, 
+      factoryNumber: req.body.factoryNumber,
+      title: req.body.title,
+      description: req.body.description
+    };
+    db.collection('lab-equip').insert(equipment, (err, result) => {
+      if (err) {
+        res.send({ 'error': 'An error occurred'});
+      } else {
+        res.send(result.ops[0]);
+      }
+    });
+  });
+
+  app.delete('/lab-equip/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('lab-equip').remove(details, (err, item) => {
+      if (err) {
+        res.send({ 'error': 'An error occurred' });
+      } else {
+        res.send('item: ' + id + ' deleted!');
+      }      
+    });
+  });
+
+  app.put ('/lab-equip/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    const equipment = { 
+      inventoryNumber: req.body.inventoryNumber, 
+      factoryNumber: req.body.factoryNumber,
+      title: req.body.title,
+      description: req.body.description
+    };
+    db.collection('lab-equip').update(details, equipment, (err, result) => {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        res.send(equipment);
+      } 
+    });
   });
 };
--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
+const db = require('./config/db');
 const app = express();
-
 const port = 8000;
-app.use(bodyParser.urlencoded({extended: true}));
-require('./app/routes')(app, {});
-app.listen(port, () => {
-  console.log('We are live on ' + port);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+MongoClient.connect(db.url, (err, dbClient) => {
+  if (err) return console.log(err);
+  console.log('Connection is okay');
+
+  const database = dbClient.db('eductech-equipment-node');
+
+  require('./app/routes')(app, database);
+  app.listen(port, () => {
+    console.log('We are live on ' + port);
+  });               
 });
